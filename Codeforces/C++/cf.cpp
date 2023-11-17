52f31cf6ca2b03235979a8db54ba5a0226a0aeff
“henryelj” <henryeljawhari@gmail.com>
2023-11-17 10:13:50 +0100

    add acripts

---
+++ Codeforces/C++/cf.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+
+// https://codeforces.com/problemset/problem/4/A
+
+// int main()                          
+// {
+//     int w{};
+//     std::cin >> w;
+//     if (w > 2 &  w % 2 == 0) {std::cout << "YES";} else {std::cout << "NO";}
+// }
+
+// https://codeforces.com/problemset/problem/71/A
+
+// int main()                          
+// {
+//     int i{};
+//     int n{};
+    
+//     std::cin >> n;
+//     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+//     std::vector<std::string> words(n);
+//     std::string line;
+    
+//     while (std::getline(std::cin, line) && i < n)
+//     {
+//         if (line.length() > 10)
+//         {
+//             line = line[0] + std::to_string(line.length() - 2) + line.back();
+//         }
+        
+//         words[i] = line;
+//         i++;
+//     }
+
+//     for (int j{0}; j < n; j++)
+//     {
+//         std::cout << words[j] << "\n";
+//     }
+
+// }
+
+// https://codeforces.com/problemset/problem/231/A
+
+// int main()                          
+// {
+//     int i{};
+//     int n{};
+//     int p1{};
+//     int p2{};
+//     int p3{};
+//     int solutions{};
+
+//     std::cin >> n;
+//     std::string line;
+    
+//     while (std::getline(std::cin, line) && i < n)
+//     {
+//         std::cin >> p1 >> p2 >> p3;
+        
+//         if (p1 + p2 + p3 > 1)
+//         {
+//             solutions++;
+//         }
+        
+//         i++;
+//     }
+
+//     std::cout << solutions << "\n";
+// }
+
+// https://codeforces.com/problemset/problem/158/A
+
+int main()                          
+{
+    int d1{};
+    int d2{};
+
+    std::cin >> d1 >> d2;
+       
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# programming_problems
+# Programming Problems
+## Henry El-Jawhari
+
+Learning mainly C++ through solving various problems on Codeforces.
